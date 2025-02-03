4431c0e86512ed04db228a0788ec862e3c0b7973
AbhiBhagwat-29 <Abhishekbhagwat45@gmail.com>
2025-02-03 23:30:45 +0530

    Database Created

--- DocNearMe/src/Database/databaseDocNearMe.sql
+++ DocNearMe/src/Database/databaseDocNearMe.sql
@@ -3,7 +3,7 @@ use DocNearMe;
 
 
 CREATE TABLE users (
-    id INT PRIMARY KEY,
+    userId INT PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     role ENUM('doctor', 'patient') NOT NULL
@@ -12,7 +12,7 @@ CREATE TABLE users (
 -- Doctors Table
 CREATE TABLE doctors (
     docterId INT PRIMARY KEY AUTO_INCREMENT,
-    userId INT AUTO_INCREMENT NOT NULL UNIQUE,
+    userId INT  NOT NULL UNIQUE,
     username VARCHAR(50) UNIQUE NOT NULL,
     full_name VARCHAR(255) NOT NULL,
     specialization VARCHAR(255) NOT NULL,
@@ -20,18 +20,18 @@ CREATE TABLE doctors (
     opening_time TIME,
     closing_time TIME,
     license BLOB NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (userId) REFERENCES users(userId)
 );
 
 -- Patients Table
 CREATE TABLE patients (
     patientId INT PRIMARY KEY AUTO_INCREMENT,
-    userId INT AUTO_INCREMENT NOT NULL UNIQUE,
+    userId INT NOT NULL UNIQUE,
     full_name VARCHAR(255) NOT NULL,
     phone_number VARCHAR(15),
     age INT,
     address TEXT,
     medical_history TEXT,
     gender ENUM('male', 'female', 'other'),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (userId) REFERENCES users(userId)
 );
