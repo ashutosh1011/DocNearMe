b1796d0269930b502221e1c3d44eda05a13e2e0f
pranilfulkar <124306139+pranilfulkar@users.noreply.github.com>
2025-02-04 19:58:46 +0530

    added dependency (jar file) required for the project

---
+++ DocNearMe/src/main/java/com/DocNearMe/DocNearMeApplication.java
@@ -0,0 +1,13 @@
+package com.DocNearMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocNearMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocNearMeApplication.class, args);
+	}
+
+}
